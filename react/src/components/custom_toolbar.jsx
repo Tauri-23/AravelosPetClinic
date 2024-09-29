511365c1fec5a93b6a0157b722f32c86786e483d
derla <96074781+wymeex@users.noreply.github.com>
2024-09-30 04:32:46 +0800

    aaa

--- react/src/components/custom_toolbar.jsx
+++ react/src/components/custom_toolbar.jsx
@@ -1,6 +1,9 @@
 // src/components/CustomToolbar.jsx
+import "../assets/css/calendar.css";
+import "../assets/css/custom_toolbar.css";
 import React from 'react';
 import PropTypes from 'prop-types';
+
 
 const CustomToolbar = ({ label, onNavigate }) => {
     const goToBack = () => {
@@ -16,10 +19,11 @@ const CustomToolbar = ({ label, onNavigate }) => {
     };
 
     return (
-        <div className="custom-toolbar">
-            <button onClick={goToBack} className="toolbar-button">Previous</button>
-            <span className="toolbar-label">{label}</span>
-            <button onClick={goToNext} className="toolbar-button">Next</button>
+        <div className="custom-toolbar d-flex justify-content-center bottom-margin-s">
+            <img src="/assets/media/icons/arrow_left.svg" className="toolbar-button arrows right-margin" alt="logo" onClick={goToBack}/>
+            <span className="toolbar-label anybody bold">{label}</span>
+            <img src="/assets/media/icons/arrow_right.svg" className="toolbar-button arrows left-margin" alt="logo" onClick={goToNext}/>
+
         </div>
     );
 };
