aa11a842dd33fced6f4bd9bf17cc6e55af0b4cd2
n0waruu <francis.evangelista.81@gmail.com>
2024-09-30 01:11:38 +0800

    update
    update

--- react/src/components/inventory_box.jsx
+++ react/src/components/inventory_box.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function InventoryBox({link}) {
     return(
         <Link to={link} className="inventory-box">
-         medicine1
+         Medicine1
          <img src="" alt="" />
         </Link>
     );
