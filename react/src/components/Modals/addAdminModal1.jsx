10d921f44609a661bb752aba3fa8d9ff359f0418
unknown <dsoliven.k11935433@umak.edu.ph>
2024-10-17 20:30:23 +0800

    doneaddadmin

--- react/src/components/Modals/addAdminModal1.jsx
+++ react/src/components/Modals/addAdminModal1.jsx
@@ -1,9 +1,137 @@
 import React from 'react'
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../assets/css/addPetModal1.css";
 
 export default function addAdminModal1({handleAddAdmin, onClose}) {
-  return (
-    <div>addAdminModal1</div>
-  )
-}
+    const [fname, setFname] = useState("");
+    const [mname, setMname] = useState("");
+    const [lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [adminDOB, setAdminDOB] = useState(null); 
+    const [Gender, setGender] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [role, setRole] = useState("");
+    const [status, setStatus] = useState("");
+    
+    const [Pic, setPic] = useState("");
+  
+    const genderOptions = [
+      { id: "male", label: "Male" },
+      { id: "female", label: "Female" },
+    ];
+  
+    const handleDateChange = (date) => {
+      setadminDOB(date);
+    };
+  
+    return (
+      <div className={`modal2`}>
+        {/* Box of modal */}
+        <div className="modal-box4">
+          <h2>Add New Admin</h2>
+          <div className="pet-info-row">
+          <input
+            type="text"
+            name="fname"
+            placeholder="First Name"
+            value={fname}
+            onChange={(e) => setFname(e.target.value)}
+          />
+          <input
+            type="text"
+            name="mname"
+            placeholder="Middle Name"
+            value={mname}
+            onChange={(e) => setMname(e.target.value)}
+          />
+          <input
+            type="text"
+            name="lname"
+            placeholder="Last Name"
+            value={lname}
+            onChange={(e) => setLname(e.target.value)}
+          />
+          </div>
+          <input
+            type="email"
+            className='pet-row-info margin-bottom'
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            className='pet-row-info margin-bottom'
+            name="password"
+            placeholder="Password"
+            value={email}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="pet-info-row">
+            <select id="viewType" value={Gender} onChange={(e) => setGender(e.target.value)}>
+              {genderOptions.map((gender) => (
+                <option key={gender.id} value={gender.id}>
+                  {gender.label}
+                </option>
+              ))}
+            </select>
+  
+            <DatePicker
+              id="adminDOB"
+              selected={adminDOB}
+              dateFormat="yyyy/MM/dd"
+              placeholderText="Birth Date"
+              onChange={handleDateChange}
+            />
+          </div>
+          <input
+            type="number"
+            className='pet-row-info margin-bottom'
+            name="phone"
+            placeholder="Phone Number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+           <input
+            type="text"
+            className='pet-row-info margin-bottom'
+            name="role"
+            placeholder="Role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          />
+          <input
+            className="pet-info-row"
+            type="file"
+            name="petPic"
+            placeholder="Pet Picture"
+            onChange={(e) => setPetPic(e.target.files[0])}
+          />
+          <div className="pet-info-row">
+          <button
+            onClick={() => {
+              const formattedDOB = petDOB ? petDOB.toISOString().split("T")[0] : null;
+              handleAddPetPost(petName, petType, petGender, formattedDOB, petBreed, petPic);
+              onClose();
+            }}
+            className="primary-btn-blue1"
+          >
+            Add Admin
+          </button>
+          <button onClick={onClose} className="sub-button">
+            Close
+          </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
 
  
